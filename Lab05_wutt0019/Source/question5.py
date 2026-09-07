3dcfd0316936bf98c586a1c355971d7c689b6c99
txngsten <oliver_wuttke@icloud.com>
2026-09-07 20:38:33 +0930

    Setup last question5.py

--- Lab05_wutt0019/Source/question5.py
+++ Lab05_wutt0019/Source/question5.py
@@ -5,3 +5,8 @@ File: question5.py
 Date: 07-09-2026
 Description:
 """
+
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import root_mean_squared_error, r2_score
