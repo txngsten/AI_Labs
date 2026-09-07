b4208859279b6c02f7bd99de19234387e3afa84d
txngsten <oliver_wuttke@icloud.com>
2026-09-07 15:16:44 +0930

    added question python files

---
+++ Lab05_wutt0019/Source/question1.py
@@ -0,0 +1,7 @@
+"""
+Student Name: Oliver Wuttke
+Student FAN: WUTT0019
+File: question1.py
+Date: 07-09-2026
+Description:
+"""
---
+++ Lab05_wutt0019/Source/question3.py
@@ -0,0 +1,7 @@
+"""
+Student Name: Oliver Wuttke
+Student FAN: WUTT0019
+File: question3.py
+Date: 07-09-2026
+Description:
+"""
